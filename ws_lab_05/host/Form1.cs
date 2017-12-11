6da3f5519796a9db2e35c23a1537804e4aac0b8f
Sergei Murkou <murkous@mail.ru>
2017-12-11 21:59:12 +0300

    add wcf host

---
+++ ws_lab_05/host/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace host {
+    public partial class Form1 : Form {
+        ServiceHost sh;
+
+        public Form1() {
+            InitializeComponent();
+            sh = new ServiceHost(typeof(ws_lab_05.Service1));
+            sh.Open();
+        }
+    }
+}
