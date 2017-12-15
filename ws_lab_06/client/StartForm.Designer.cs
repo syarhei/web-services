2bca3427b3394f41d6235fd7c76a580a50118d8e
Sergei Murkou <murkous@mail.ru>
2017-12-16 00:18:25 +0300

    add client for wcf data service

---
+++ ws_lab_06/client/StartForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace client {
+    partial class StartForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.result = new System.Windows.Forms.RichTextBox();
+            this.getStudents = new System.Windows.Forms.Button();
+            this.getNotes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(12, 12);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(693, 293);
+            this.result.TabIndex = 0;
+            this.result.Text = "";
+            // 
+            // getStudents
+            // 
+            this.getStudents.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.getStudents.Location = new System.Drawing.Point(12, 311);
+            this.getStudents.Name = "getStudents";
+            this.getStudents.Size = new System.Drawing.Size(342, 71);
+            this.getStudents.TabIndex = 1;
+            this.getStudents.Text = "students";
+            this.getStudents.UseVisualStyleBackColor = true;
+            this.getStudents.Click += new System.EventHandler(this.getStudents_Click);
+            // 
+            // getNotes
+            // 
+            this.getNotes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.getNotes.Location = new System.Drawing.Point(360, 311);
+            this.getNotes.Name = "getNotes";
+            this.getNotes.Size = new System.Drawing.Size(345, 71);
+            this.getNotes.TabIndex = 2;
+            this.getNotes.Text = "notes";
+            this.getNotes.UseVisualStyleBackColor = true;
+            this.getNotes.Click += new System.EventHandler(this.getNotes_Click);
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 394);
+            this.Controls.Add(this.getNotes);
+            this.Controls.Add(this.getStudents);
+            this.Controls.Add(this.result);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "StartForm";
+            this.Text = "StartForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox result;
+        private System.Windows.Forms.Button getStudents;
+        private System.Windows.Forms.Button getNotes;
+    }
+}
---
+++ ws_lab_06/client/StartForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client {
+    public partial class StartForm : Form {
+        MSUWS.WSEntities entity;
+
+        public StartForm() {
+
+            entity = new MSUWS.WSEntities(new Uri("http://localhost:54570/MSUService.svc/"));
+
+            InitializeComponent();
+        }
+
+        private void getStudents_Click(object sender, EventArgs e) {
+            result.Text = "";
+            foreach (MSUWS.student student in entity.students.AsEnumerable()) {
+                result.Text += string.Format("Student: '{0}' (id:{1})\n", student.name, student.id);
+            }
+        }
+
+        private void getNotes_Click(object sender, EventArgs e) {
+            List<MSUWS.note> notes = entity.notes.Where(n => n.notes >= 4).ToList();
+
+            result.Text = "";
+            foreach (MSUWS.note note in notes) {
+                result.Text += string.Format("Note: {0} on exam {1} (id student:{2})\n", note.notes, note.subject, note.id);
+            }
+        }
+    }
+}
