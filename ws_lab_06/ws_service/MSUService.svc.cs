e5b2db1a0640ad68b759135e588c71372acf420f
Sergei Murkou <murkous@mail.ru>
2017-12-16 11:47:26 +0300

    update access rule for all crud operations

--- ws_lab_06/ws_service/MSUService.svc.cs
+++ ws_lab_06/ws_service/MSUService.svc.cs
@@ -17,7 +17,7 @@ namespace ws_service {
         public static void InitializeService(DataServiceConfiguration config) {
             // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
             // Examples:
-            config.SetEntitySetAccessRule("*", EntitySetRights.AllRead);
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
             // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
             config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
         }
