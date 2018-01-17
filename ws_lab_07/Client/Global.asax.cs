83bd91f07609814a70af4afe23c1f23c4356cb36
Sergei Murkou <murkous@mail.ru>
2018-01-18 01:14:02 +0300

    add client for wcf syndication service

---
+++ ws_lab_07/Client/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Client
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Uri baseAddress = new Uri("http://localhost:59295/Service");
+            WebServiceHost svcHost = new WebServiceHost(typeof(Feed1), baseAddress);
+            svcHost.Open();
+        }
+    }
+}
